604ff5e325dac520722a19acb5f2a9f515a2368f
kizzlesulli <kysulli+git@gmail.com>
2022-08-31 21:20:56 -0600

    Add start round function to change HTML to start of game

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
+//Change intro HTML to cancel game HTML
+
+function cancelGame(scenario, buttonRight, resultsText) {
+
+    scenario.innerText = 'The unhinged rock paper scissors enthusiast chose scissors...';
+    
+    buttonRight.classList.remove('cancel')
+    buttonRight.classList.add('chose');;
+    resultsText.innerText = 'Hope you run fast...';
+}
 
 //Change intro HTML to ask number of rounds HTML
 
-function askNumberRounds() {
-
-    let scenario = document.querySelector('.scenario');
-    let numInput = document.querySelector('.box.play');
-    let startButton = document.querySelector('.box.cancel');
+function askNumberRounds(scenario, buttonLeft, buttonRight) {
 
     scenario.innerText = 'How many games do you want to play?';
     
-    numInput.innerHTML = '<input class="get-number" type="number" step="2" min="3" value="3">';
-    numInput.classList.remove('play');
-    numInput.classList.add('number-games');
-    startButton.innerText = 'START';
-    startButton.classList.remove('cancel')
-    startButton.classList.add('start');
-    console.log(numInput.classList, startButton.classList);
+    buttonLeft.innerHTML = '<input class="get-rounds" type="number" step="2" min="3" value="3">';
+    buttonLeft.classList.remove('play');
+    buttonLeft.classList.add('number-rounds');
+    buttonRight.innerText = 'START';
+    buttonRight.classList.remove('cancel')
+    buttonRight.classList.add('start');;
+}
+
+//Change ask number of rounds HTML to start game HTML
+
+let numberOfRounds;
+
+function startGame(scenario, buttonLeft, buttonRight, resultsText, roundNumber) {
+
+    scenario.remove();
+    buttonLeft.innerText = '';
+    buttonRight.innerText = '';
+    resultsText.innerText = '';
+    roundNumber.innerText = 'Round 1';
+
+    let playerResults = document.querySelector('.player-results');
+    let computerResults = document.querySelector('.computer-results');
+    let playerSelection = document.querySelector('.player-selection');
+
+    playerResults.innerHTML = '';
+    computerResults.innerHTML = '';
+
+    playerResults.innerHTML = `
+        <div class="player-name">Distressed Defender</div>
+        <div class="player-score">0</div>
+        <p></p>
+        <div class="box player-choice"></div>
+    `;
+
+    computerResults.innerHTML = `
+        <div class="computer-name">Unhinged Enthusiast</div>
+        <div class="computer-score">0</div>
+        <p></p>
+        <div class="box computer-choice"></div>
+    `;
+
+    playerSelection.innerHTML = `
+        <div class="selection-text">How will you defend yourself?</div>
+        <div class="selection-boxes">
+            <div class="selection box rock">Rock</div>
+            <div class="selection box paper">Paper</div>
+            <div class="selection box scissors">Scissors</div>
+        </div>
+    `;
 }
 
 
@@ -130,12 +178,30 @@ function playGame(num) {
 
 //playGame(numberOfGames);
 
-const boxes = Array.from(document.getElementsByClassName('box'));
-boxes.forEach(box => box.addEventListener('click', (e) => {
+const topBoxes = document.querySelectorAll('.box');
+topBoxes.forEach(box => box.addEventListener('click', (e) => {
 
-    if(e.target.classList.contains('play')) {
-        askNumberRounds();
-    };
+    console.log(e.target);
+
+    let scenario = document.querySelector('.scenario');
+    let buttonLeft = document.querySelector('.box.top-left');
+    let buttonRight = document.querySelector('.box.top-right');
+    let resultsText = document.querySelector('.results-text');
+    let roundNumber = document.querySelector('.round-number');
+
+    if(e.target.classList.contains('cancel')) {
+        cancelGame(scenario, buttonRight, resultsText);
+
+    } else if(e.target.classList.contains('play')) {
+        askNumberRounds(scenario, buttonLeft, buttonRight);
+
+    } else if (e.target.classList.contains('start')) {
+        
+        numberOfRounds = document.querySelector('.get-rounds').value;
+        startGame(scenario, buttonLeft, buttonRight, resultsText, roundNumber);
+        //playGame(numberOfRounds);
+    }
+
 }));
 
 
