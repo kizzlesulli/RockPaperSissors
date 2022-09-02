5ebc28b27bedc5c3c3d07478cf96a0ee9d93abde
kizzlesulli <kysulli+git@gmail.com>
2022-09-01 22:22:37 -0600

    Edit dom manipulation to preserve event listeners Add results display Add declare winner Add clear game Add play again

--- script.js
+++ script.js
@@ -1,65 +1,76 @@
+const divs = {
+    roundNumber: document.querySelector('.round-number'),
+    buttonLeft: document.querySelector('.box.top-left'),
+    buttonRight: document.querySelector('.box.top-right'),
+    playerResults: document.querySelector('.player-results'),
+    playerName: document.querySelector('.player-name'),
+    playerScore: document.querySelector('.player-score'),
+    playerText: document.querySelector('.player-results > p'),
+    computerResults: document.querySelector('.computer-results'),
+    computerName: document.querySelector('.computer-name'),
+    computerScore: document.querySelector('.computer-score'),
+    computerText: document.querySelector('.computer-results > p'),
+    resultsText: document.querySelector('.results-text'),
+    playerSelection: document.querySelector('.player-selection'),
+}
+
 //Change intro HTML to cancel game HTML
 
-function cancelGame(roundNumber, buttonRight, resultsText) {
+function cancelGame() {
 
-    roundNumber.innerText = 'The unhinged rock paper scissors enthusiast chose scissors...';
+    divs.roundNumber.innerText = 'The unhinged rock paper scissors enthusiast chose scissors...';
     
-    buttonRight.classList.remove('cancel')
-    buttonRight.classList.add('chose');;
-    resultsText.innerText = 'Hope you run fast...';
+    divs.buttonRight.classList.remove('cancel')
+    divs.buttonRight.classList.add('chose');;
+    divs.resultsText.innerText = 'Hope you run fast...';
 }
 
 //Change intro HTML to ask number of rounds HTML
 
-function askNumberRounds(roundNumber, buttonLeft, buttonRight, resultsText) {
+function askNumberRounds() {
 
-    resultsText.innerHTML = '';
-    roundNumber.innerText = 'How many games do you want to play?';
+    //Removes highlight if deciding to play after canceling
+    divs.buttonRight.classList.remove('chose');;
     
-    buttonLeft.innerHTML = '<input class="get-rounds" type="number" step="2" min="3" value="3">';
-    buttonLeft.classList.remove('play');
-    buttonLeft.classList.add('number-rounds');
-    buttonRight.innerText = 'START';
-    buttonRight.classList.remove('cancel')
-    buttonRight.classList.add('start');;
+    divs.resultsText.innerText = '';
+    divs.roundNumber.innerText = 'How many rounds do you want to play?';
+    
+    divs.buttonLeft.innerHTML = '<input class="get-rounds" type="number" step="2" min="3" value="3">';
+    divs.buttonLeft.classList.remove('play');
+    divs.buttonLeft.classList.add('number-rounds');
+    divs.buttonRight.innerText = 'START';
+    divs.buttonRight.classList.remove('cancel')
+    divs.buttonRight.classList.add('start');;
 }
 
 //Change ask number of rounds HTML to start game HTML
 
-let numberOfRounds;
+function startGame() {
 
-function startGame(roundNumber, buttonLeft, buttonRight, playerSelection, resultsText) {
-
-    roundNumber.classList.remove('scenario');
-    roundNumber.innerText = 'Round 1';
+    divs.roundNumber.classList.remove('scenario');
+    divs.roundNumber.innerText = 'Round 1';
     
-    buttonLeft.innerHTML = '';
-    buttonLeft.classList.remove('number-rounds');
-    buttonLeft.classList.add('player-choice');
+    divs.buttonLeft.innerHTML = '';
+    divs.buttonLeft.classList.remove('number-rounds');
+    divs.buttonLeft.classList.add('player-choice');
     
-    buttonRight.innerText = '';
-    buttonRight.classList.remove('start');
-    buttonRight.classList.add('computer-choice');
+    divs.buttonRight.innerText = '';
+    divs.buttonRight.classList.remove('start');
+    divs.buttonRight.classList.add('computer-choice');
     
-    resultsText.innerText = '';
+    divs.resultsText.innerText = '';
 
-    let playerName = document.querySelector('.player-name');
-    let playerScore = document.querySelector('.player-score');
-    let playerText = document.querySelector('.player-results > p');
+    divs.playerName.innerText = 'Distressed Defender';
+    divs.playerScore.innerText = '0';
+    divs.playerText.innerText = 'You chose...';
 
-    playerName.innerText = 'Distressed Defender';
-    playerScore.innerText = '0';
-    playerText.innerText = 'You chose...';
+    divs.computerName.innerText = 'Unhinged Enthusiast';
+    divs.computerScore.innerText = '0';
+    divs.computerText.innerText = 'They chose...';
 
-    let computerName = document.querySelector('.computer-name');
-    let computerScore = document.querySelector('.computer-score');
-    let computerText = document.querySelector('.computer-results > p');
-
-    computerName.innerText = 'Unhinged Enthusiast';
-    computerScore.innerText = '0';
-    computerText.innerText = 'They chose...';
-
-    playerSelection.innerHTML = `
+    //selection-box event listeners added in playGame function 
+    //player-selection innerHTML doesn't need to persist
+    divs.playerSelection.innerHTML = `
         <div class="selection-text">How will you defend yourself?</div>
         <div class="selection-boxes">
             <div class="selection box rock">Rock</div>
@@ -69,35 +80,32 @@ function startGame(roundNumber, buttonLeft, buttonRight, playerSelection, result
     `;
 }
 
-//Get player choice of Rock, Paper, or Scissors
-//Make sure it is Rock, Paper, or Scissors
-//Ask to choose again if it isn't and take that choice
 
-function getPlayerChoice(choice) {
+//Play game/round variables
+let currentRound = 1;
+let playerScore = 0;
+let computerScore = 0;
 
-    if (choice == 'Rock') {
-        
-        console.log('You choose Rock.');
-    
-    } else if (choice == 'Paper') {
-        
-        console.log('You choose Paper.');
-    
-    } else if (choice == 'Scissors') {
-        
-        console.log('You choose Scissors.');
-    }
-    
-    return choice.charAt(0).toUpperCase() + choice.slice(1);
+function clearGame() {
+
+    currentRound = 1;
+    playerScore = 0;
+    computerScore = 0;
+
+    divs.playerSelection.innerHTML = '';
+
+    divs.playerName.innerText = '';
+    divs.playerScore.innerText = '';
+    divs.playerText.innerText = '';
+
+    divs.computerName.innerText = '';
+    divs.computerScore.innerText = '';
+    divs.computerText.innerText = '';
 }
-
-//Random number between 0 and 2
 
 function getRandThree() {
     return Math.floor(Math.random() * 3)
 }
-
-//Get computer choice of Rock, Paper, or Scissors
 
 function getComputerChoice() {
     
@@ -111,132 +119,120 @@ function getComputerChoice() {
         case 2:
             return 'Scissors';
     }
-
-    // if (n == 0) {
-    //     console.log('Computer chooses Rock.');
-    //     return 'Rock';
-   
-    // } else if (n == 1) {
-    //     console.log('Computer chooses Paper.');
-    //     return 'Paper';
-   
-    // } else if (n == 2) {
-    //     console.log('Computer chooses Scissors.');
-    //     return 'Scissors';
-    // }
 }
-
-//Play a round of Rock, Paper, Scissors
-//Check player vs computer choice
-//Declare winner
 
 function playRound(player, computer) {
 
     if (player == computer) {
         
-        console.log(`It\'s a tie! You both chose ${player}.`);
+        return `Round ${currentRound} is a a tie! You both chose ${player}.`;
     
     } else if ((player == 'Rock' && computer == 'Scissors') ||
                 (player == 'Paper' && computer == 'Rock') ||
                 (player == 'Scissors' && computer == 'Paper')) {
         
-        console.log(`You win! ${player} beats ${computer}`); 
+        playerScore++;
+        return `You win Round ${currentRound}! ${player} beats ${computer}`; 
     
     } else if ((computer == 'Rock' && player == 'Scissors') ||
                 (computer == 'Paper' && player == 'Rock') ||
                 (computer == 'Scissors' && player == 'Paper')) {
         
-        console.log(`You Lose! ${computer} beats ${player}`);  
+        computerScore++;
+        return `You Lose Round ${currentRound}! ${computer} beats ${player}`;  
     }
 }
 
-//Start the game and iterate rounds
-//Let player know game is over
+function decideWinner() {
 
-let i = 1;
-let playerScore = 0;
-let computerScore = 0;
+    if (playerScore > computerScore) {
 
-function playGame(numberOfRounds, roundNumber, buttonLeft, buttonRight, resultsText) {
+        return 'You won! The unhinged enthusiast begins to question their life.'
+
+    } else if (playerScore < computerScore) {
+
+        return 'You lost... The unhinged enthusiast isn\' putting away their scissors.'
+    } else if (playerScore == computerScore) {
+
+        return 'It\'s a tie. The unhinged enthusiast doesn\'t appear to accept this result.'
+    }
+}
+
+function playGame(numberOfRounds) {
 
     let selectionText = document.querySelector('.selection-text');
-    let selectionBoxesDiv = document.querySelector('.selection-boxes');
+    let buttonRock = document.querySelector('.box.rock');
+    let buttonPaper = document.querySelector('.box.paper');
+    let buttonScissors = document.querySelector('.box.scissors');
     let computerChoice;
     let playerChoice;
 
     const selectionBoxes = document.querySelectorAll('.selection.box')
     selectionBoxes.forEach(box => box.addEventListener('click', (e) => {
 
-        if (i < numberOfRounds) {
+        if (e.target.classList.contains('play-again')) {
+           
+            clearGame();
+
+            divs.roundNumber.classList.add('scenario');
+            divs.buttonLeft.classList.add('start');
             
+            askNumberRounds();
+        
+        } else if (currentRound < numberOfRounds) { 
             playerChoice = e.target.innerText;
             computerChoice = getComputerChoice();
-            console.log(buttonLeft);
-            buttonLeft.innerText = `${playerChoice}`;
-            buttonRight.innerText = `${computerChoice}`;
+            results = playRound(playerChoice, computerChoice);
             
-            i++
-            roundNumber.innerText = `Round ${i}`;
-
+            divs.buttonLeft.innerText = playerChoice;
+            divs.buttonRight.innerText = computerChoice;
+            divs.resultsText.innerText =  results;
+            divs.playerScore.innerText = playerScore;
+            divs.computerScore.innerHTML = computerScore;
+            
+            currentRound++
+            divs.roundNumber.innerText = `Round ${i}`;
         } else {
-            
+        
         //Play last round
+            playerChoice = e.target.innerText;
             computerChoice = getComputerChoice();
-
-            buttonLeft.innerText = `${e.target.innerText}`;
-            buttonRight.innerText = `${computerChoice}`;
-            roundNumber.innerHTML = 'Game Over';
+            results = playRound(playerChoice, computerChoice, currentRound);
+        
+            divs.buttonLeft.innerText = playerChoice;
+            divs.buttonRight.innerText = computerChoice;
+            divs.resultsText.innerText =  results;
+            divs.playerScore.innerText = playerScore;
+            divs.computerScore.innerHTML = computerScore;
+            
+            divs.roundNumber.innerText = decideWinner();
 
         //End game and ask to play again
+            buttonRock.remove();
+            buttonScissors.remove();
+            buttonPaper.classList.remove('paper');
+            buttonPaper.classList.add('play-again');
             selectionText.innerText = 'It does\'t look like they are going to leave you alone...';
-            selectionBoxesDiv.innerHTML = '<div class="selection box play-again">Play Again?</div>';
-
-            if (e.target.classList.contains('play-again')) {
-                i = 1;
-                roundNumber.classList.add('scenario');
-                askNumberRounds(roundNumber, buttonLeft, buttonRight);
-            }
+            buttonPaper.innerText = 'Play again?';
         }
-
-    }))
-
-    // for(i = 1; i < numberOfRounds; i++) {
-
-    //     roundNumber.innerText = `Round ${i}`;
-
-    //     let playerChoice = getPlayerChoice();
-    //     let computerChoice = getComputerChoice();
-
-    //     playRound(playerChoice, computerChoice);
-    // }
-
-    // console.log(`That\'s ${i} rounds. Thanks for playing.`)
+    }
+    ));
 }
-
-//playGame(numberOfGames);
 
 const topBoxes = document.querySelectorAll('.box');
 topBoxes.forEach(box => box.addEventListener('click', (e) => {
 
-    let roundNumber = document.querySelector('.round-number');
-    let buttonLeft = document.querySelector('.box.top-left');
-    let buttonRight = document.querySelector('.box.top-right');
-    let resultsText = document.querySelector('.results-text');
-    let playerSelection = document.querySelector('.player-selection');
-
     if(e.target.classList.contains('cancel')) {
-        cancelGame(roundNumber, buttonRight, resultsText);
+        cancelGame();
 
     } else if(e.target.classList.contains('play')) {
-        askNumberRounds(roundNumber, buttonLeft, buttonRight, resultsText);
+        askNumberRounds();
 
     } else if (e.target.classList.contains('start')) {
         
-        numberOfRounds = document.querySelector('.get-rounds').value;
-        startGame(roundNumber, buttonLeft, buttonRight, playerSelection, resultsText);
-        playGame(numberOfRounds, roundNumber, buttonLeft, buttonRight, resultsText);
+        let numberOfRounds = document.querySelector('.get-rounds').value;
+        
+        startGame();
+        playGame(numberOfRounds);
     }
 }));
-
-
-//window.addEventListener('click', e => console.log(e.target));
